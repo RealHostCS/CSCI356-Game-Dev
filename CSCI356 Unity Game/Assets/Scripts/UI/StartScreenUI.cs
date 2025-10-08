a894eb4629ab43bbbc6c5afd4aea87647f724f04
RedFx8864 <redflame1980liver@gmail.com>
2025-10-08 20:03:59 +1100

    Basic Level Select

--- CSCI356 Unity Game/Assets/Scripts/UI/StartScreenUI.cs
+++ CSCI356 Unity Game/Assets/Scripts/UI/StartScreenUI.cs
@@ -1,13 +1,36 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 public class StartScreenUI : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject StartScreen;
+    public GameObject LevelSelectScreen;
 
+    public GameObject LastScreen;
+    public GameObject CurrentScreen;
     public void StartGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        StartScreen.SetActive(false);
+        LevelSelectScreen.SetActive(true);
+        LastScreen = StartScreen;
+        CurrentScreen = LevelSelectScreen;
 
+    }
+
+    public void SelectScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Back()
+    {
+        LastScreen.SetActive(true);
+        CurrentScreen.SetActive(false);
+        GameObject x = LastScreen;
+        LastScreen = CurrentScreen;
+        CurrentScreen = x;
+    
     }
 }
